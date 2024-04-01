dcbcca64e4548368a1bbb4839f2ad7dbde359153
yashipawar <yashashri2811@gmail.com>
2024-04-01 19:56:12 +0530

    external databse

--- Backend/src/config/pool.js
+++ Backend/src/config/pool.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false // Set to false to accept self-signed certificates; for development only!
+  }
 });
 
 pool.connect((err, client, release) => {
